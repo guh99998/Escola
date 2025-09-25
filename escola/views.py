134e75ec4361f856603b8e5cab2e91cc9d35b66d
Gustavo Lopes <guhrlopes999@gmail.com>
2025-09-25 11:33:38 -0300

    feat: Criado viewset de Matricula e views

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
-from escola.models import Estudante, Curso
-from escola.serializers import EstudanteSerializer, CursoSerializer
+from rest_framework import viewsets, generics
+from escola.models import Estudante, Curso, Matricula
+from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasCursoSerializer, ListaMatriculasEstudanteSerializer
 
 
 class EstudanteViewSet(viewsets.ModelViewSet):
@@ -10,3 +10,19 @@ class EstudanteViewSet(viewsets.ModelViewSet):
 class CursoViewSet(viewsets.ModelViewSet):
   queryset = Curso.objects.all()
   serializer_class = CursoSerializer
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+  queryset = Matricula.objects.all()
+  serializer_class = MatriculaSerializer
+
+class ListaMatriculaEstudante(generics.ListAPIView):
+  def get_queryset(self):
+    queryset = Matricula.objects.filter(estudante_id = self.kwargs['pk'])
+    return queryset
+  serializer_class = ListaMatriculasEstudanteSerializer
+
+class ListaMatriculaCurso(generics.ListAPIView):
+  def get_queryset(self):
+    queryset = Matricula.objects.filter(curso_id = self.kwargs['pk'])
+    return queryset
+  serializer_class = ListaMatriculasCursoSerializer
